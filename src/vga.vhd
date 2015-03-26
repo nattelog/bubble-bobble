44049a105f0963074e2e836a7d457db3d5c64907
Natanael Log <natlo809@student.liu.se>
2015-03-26 14:33:46 +0100

    Added some basic files

---
+++ src/vga.vhd
@@ -0,0 +1,20 @@
+-- The VGA-controller
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity cpu is
+    Port ( clk,rst : in  STD_LOGIC;
+        vgaRed : out STD_LOGIC_VECTOR(2 downto 0);
+        vgaGreen : out STD_LOGIC_VECTOR(2 downto 0);
+    	vgaBlue : out STD_LOGIC_VECTOR(1 downto 0);
+    	Hsync : out STD_LOGIC;
+    	Vsync : out STD_LOGIC;
+    );
+end cpu;
+
+architecture behavioral of cpu is
+begin
+
+end behavioral;
