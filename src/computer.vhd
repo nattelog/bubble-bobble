74504858e72211258cb59db790eccf5bca96fa11
Natanael Log <natlo809@student.liu.se>
2015-03-26 15:36:58 +0100

    Some changes...

--- src/computer.vhd
+++ src/computer.vhd
@@ -4,7 +4,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity cpu is
+entity computer is
     Port ( clk,rst : in  STD_LOGIC;
     	vgaRed : out STD_LOGIC_VECTOR(2 downto 0);
         vgaGreen : out STD_LOGIC_VECTOR(2 downto 0);
@@ -12,9 +12,9 @@ entity cpu is
     	Hsync : out STD_LOGIC;
     	Vsync : out STD_LOGIC;
     );
-end cpu;
+end computer;
 
-architecture behavioral of cpu is
+architecture behavioral of computer is
 begin
 
 end behavioral;
