eb1f7952337a90414804866b635a9704d8ad6339
Natanael Log <natlo809@student.liu.se>
2015-03-26 16:55:22 +0100

    Changed in computer.vhd

--- src/computer.vhd
+++ src/computer.vhd
@@ -5,16 +5,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity computer is
-    Port ( clk,rst : in  STD_LOGIC;
-    	vgaRed : out STD_LOGIC_VECTOR(2 downto 0);
-        vgaGreen : out STD_LOGIC_VECTOR(2 downto 0);
-    	vgaBlue : out STD_LOGIC_VECTOR(1 downto 0);
-    	Hsync : out STD_LOGIC;
-    	Vsync : out STD_LOGIC
+    Port ( clk : in  STD_LOGIC
     );
 end computer;
 
 architecture behavioral of computer is
 begin
+
+process (clk) begin
+	if rising_edge(clk) then
+	end if;
+end process;
 
 end behavioral;
