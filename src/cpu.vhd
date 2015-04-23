3145ea9f6578e36b0589442370fd64f2efdc3420
Natanael Log <natlo809@student.liu.se>
2015-04-23 15:24:56 +0200

    Jobbar

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -17,36 +17,27 @@ architecture behavioral of cpu is
   signal ASR : STD_LOGIC_VECTOR(15 downto 0);
 
   -- Memory Unit
+  signal pm_write : STD_LOGIC;
   type pm_t is array(0 to 255) of STD_LOGIC_VECTOR(15 downto 0);
   signal PM : pm_t := (
     X"1100",
     others => X"0000"
-);
+    );
 
-  -- Signals from programword
-  --alias op : STD_LOGIC_VECTOR(3 downto 0) is PM(31 downto 28);
-  --alias gradr : STD_LOGIC_VECTOR(3 downto 0) is PR(27 downto 24);
-  --alias m : STD_LOGIC_VECTOR(1 downto 0) is PM(23 downto 22);
-  --alias padr : STD_LOGIC_VECTOR(15 downto 0) is PM(15 downto 0);
+  -- Signals from programword/dataregister
+  alias dr_op : STD_LOGIC_VECTOR(3 downto 0) is DR(15 downto 12);
+  alias dr_grx : STD_LOGIC_VECTOR(3 downto 0) is DR(11 downto 8);
+  alias dr_m : STD_LOGIC_VECTOR(1 downto 0) is DR(7 downto 6);
 
-  -- Control Unit
-  signal mPC : STD_LOGIC_VECTOR(6 downto 0);
+  -- Control Unit signals
+  signal mPC, k1, k2 : STD_LOGIC_VECTOR(7 downto 0);
   signal CONTROLWORD : STD_LOGIC_VECTOR(0 to 23);
   
   component micro_memory is
     port (clk, rst : in STD_LOGIC;
-          adr : in STD_LOGIC_VECTOR(6 downto 0);
+          adr : in STD_LOGIC_VECTOR(7 downto 0);
           controlword : out STD_LOGIC_VECTOR(0 to 23));
   end component;
-
-  -- K-registers
-  type k_t is array(0 to 15) of STD_LOGIC_VECTOR(6 downto 0);
-
-  constant k1 : k_t := (
-    0,
-    1,
-    others => 0
-    );
 
   -- Signals from controlword
   --alias operation : STD_LOGIC_VECTOR(3 downto 0) is controlword(0 to 3);
@@ -56,6 +47,23 @@ architecture behavioral of cpu is
   --alias lc : STD_LOGIC_VECTOR(1 downto 0) is controlword(11 to 12);
   --alias seq : STD_LOGIC_VECTOR(3 downto 0) is controlword(13 to 16);
   --alias madr : STD_LOGIC_VECTOR(6 downto 0) is controlword(17 to 23);
+
+  -- K-registers
+  type k_t is array(0 to 15) of STD_LOGIC_VECTOR(7 downto 0);
+
+  constant k1_reg : k_t := (
+    X"00",
+    others => X"00"
+    );
+
+  constant k2_reg : k_t := (
+    x"00",
+    others => X"00"
+    );
+
+  -- General registers
+  type gr_t is array(0 to 15) of STD_LOGIC_VECTOR(15 downto 0);
+  signal gen_reg : gr_t;
   
 begin
 
@@ -64,6 +72,7 @@ begin
   -- **********
 
   BUSS <= DR when tb = "010" else
+          GR when tb = "110" else
           (others => '0');
 
   
@@ -72,6 +81,22 @@ begin
   -- ******************
 
   mm : micro_memory port map(clk, rst, mPC, CONTROLWORD);
+
+  -- K registers
+  k_reg : process (clk)
+  begin
+    if rising_edge(clk) then
+      if (rst = '1') then
+        k1 <= (others => '0');
+        k2 <= (others => '0');
+
+      else
+        k1 <= k1_reg(CONV_INTEGER(dr_op));
+        k2 <= k2_reg(CONV_INTEGER(dr_m));
+        
+      end if;
+    end if;
+  end process;
 
   process (clk)
   begin
@@ -88,12 +113,17 @@ begin
   -- ** MEMORY UNIT **
   -- *****************
 
-  process (clk)
+  primary_memory : process (clk)
   begin
     if rising_edge(clk) then
       if (rst = '1') then
         DR <= (others => '0');
         ASR <= (others => '0');
+        pm_write <= '0';
+
+      else
+        
+        
       end if;
     end if;
   end process;
--- src/micro_memory.vhd
+++ src/micro_memory.vhd
@@ -6,18 +6,18 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity micro_memory is
   Port (clk, rst : in STD_LOGIC;
-        adr : in STD_LOGIC_VECTOR(6 downto 0);
+        adr : in STD_LOGIC_VECTOR(7 downto 0);
         controlword : out STD_LOGIC_VECTOR(0 to 23));
 end micro_memory;
 
 architecture behavioral of micro_memory is
 
-  type mm_t is array(0 to 127) of STD_LOGIC_VECTOR(0 to 23);
+  type mm_t is array(0 to 256) of STD_LOGIC_VECTOR(0 to 23);
 
 
   -- Example microinstructions
   --
-  --  
+  -- 
   
   constant MM : mm_t := (
     X"111000",
