f09ae7b71152c61b613651a3ee044657c0d93990
Natanael Log <natlo809@student.liu.se>
2015-04-23 16:44:47 +0200

    Jobbar

--- src/micro_memory.vhd
+++ src/micro_memory.vhd
@@ -22,7 +22,7 @@ architecture behavioral of micro_memory is
   constant DR_TB : STD_LOGIC_VECTOR(2 downto 0) := "010";
   
   constant MM : mm_t := (
-    "0000" & DR_FT & "00000000000000000",
+    "0000" & DR_TB & "00000000000000000",
     others => (others => '0')
     );
   
