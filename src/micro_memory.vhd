e2b74d0de7a5fce4f9b53bad42808619e59ad33b
Natanael Log <natlo809@student.liu.se>
2015-04-24 16:39:15 +0200

    Jobbar

--- src/micro_memory.vhd
+++ src/micro_memory.vhd
@@ -71,8 +71,8 @@ architecture behavioral of micro_memory is
   constant MM : mm_t := (
     ALU_FB & TB_DR & FB_GR & P & LC & SEQ & MADR,
     ALU_LSR & TB_AR & FB & P & LC & SEQ & MADR,
-    ALU_LSR & TB_PC & FB_ASR & P_INC & LC & SEQ & MADR,
-    ALU_LSR & TB_DR & FB & P & LC & SEQ & MADR,
+    ALU_LSR & TB & FB & P_INC & LC & SEQ & MADR,
+    ALU_LSR & TB_PC & FB_ASR & P & LC & SEQ & MADR,
     ALU_LSR & TB & FB & P & LC & SEQ & MADR,
     ALU_LSR & TB & FB & P & LC & SEQ & MADR,
     ALU_LSR & TB & FB & P & LC & SEQ & MADR,
