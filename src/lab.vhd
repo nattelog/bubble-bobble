ef277f82c306a0c76c3d94d7ac14c6ba7305ff84
Oscar Johansson <oscjo411@student.liu.se>
2015-04-28 16:59:02 +0200

    test

--- src/lab.vhd
+++ src/lab.vhd
@@ -228,7 +228,7 @@ begin
          red(2 downto 0) <= "111";
          green(2 downto 0) <= "111";
          blue(2 downto 1) <= "11";
-       when '2' =>
+       when "10" =>
          red(2 downto 0) <= "000";
          green(2 downto 0) <= "111";
          blue(2 downto 1) <= "000";
