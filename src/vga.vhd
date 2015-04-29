e7ea825690f37091273abe6a9e434a97c14fbe95
Oscar Johansson <oscjo411@student.liu.se>
2015-04-29 10:29:16 +0200

    test

--- src/vga.vhd
+++ src/vga.vhd
@@ -1,4 +1,6 @@
 -- The VGA-controller
+-- Used the timing values on page 7; http://lslwww.epfl.ch/pages/teaching/cours_lsl/ca_es/VGA.pdf
+-- This site gives slight different values? http://martin.hinner.info/vga/timing.html ie sync pulse 2
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -6,86 +8,158 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
  
 
 entity vga is
-    Port ( clk,rst : in  STD_LOGIC;
-        vgaRed : out STD_LOGIC_VECTOR(2 downto 0);
-        vgaGreen : out STD_LOGIC_VECTOR(2 downto 0);
-    	vgaBlue : out STD_LOGIC_VECTOR(1 downto 0);
-    	Hsync : out STD_LOGIC;
-    	Vsync : out STD_LOGIC
-    );
+
+  port  ( clk,rst : in  STD_LOGIC;
+        vga_red : out STD_LOGIC_VECTOR(2 downto 0);
+        vga_green : out STD_LOGIC_VECTOR(2 downto 0);
+    	   vga_blue : out STD_LOGIC_VECTOR(2 downto 1);
+    	   h_sync : out STD_LOGIC;
+    	   v_sync : out STD_LOGIC
+        );
+
 end vga;
 
-
-
 architecture behavioral of vga is
-  component gm
-    port( adress : in STD_LOGIC_VECTOR(15 downto 0);
-          data : out STD_LOGIC_VECTOR(31 downto 0)
-          );
-  end component;
-  signal a : out STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";
-  signal d : in STD_LOGIC_VECTOR(31 downto 0);
-  variable countPixel : integer := 0; -- För att kunna dela upp vectorn i gm i 4.
-  variable clkCnt : integer := 0; --räknar 0-3 varje klockcykel, för att tima
-                                  --med skärm
-  variable waitRow : integer := 0; --räknar väntetiden mellan rader
-  variable waitCol : integer := 0; --räknar väntetiden efter alla rader
-  variable pixelSent : integer := 0; --Räknar pixlar man skickat per rad
-  variable RGB : boolean =  true; --är true om vi håller på att ta in/skickar pixlar
-  variable rowCnt : integer := 0; -- räknar antal rader vi skrivit
-                                
+
+-- ************
+-- **  VGA   ** 
+-- ************
+
+    signal xctr, yctr : integer := 0;                     -- 40 ns per pixel/count
+    signal pixel : std_logic_vector (1 downto 0) := "00"; -- Our clock is 100 MHz => Wait 4
+    signal hs : std_logic := '1';
+    signal vs : std_logic := '1';
+    
+-- ************
+-- **  GMEM  **
+-- ************
+
+    type tile_t is array(0 to 15) of STD_LOGIC_VECTOR(15 downto 0);
+    
+    signal tile_block : tile_t  := (
+      others => (others => '0')
+    );
+    
+
+-- *******************
+-- ** SPRITES & MAP **
+-- *******************
+    type map_t is array(0 to 29) of STD_LOGIC_VECTOR(0 to 39);
+    signal game_map : map_t := (
+    "1111111111111110001111111000111111111111",
+    "1111111111111110001111111000111111111111",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000111111111110000000000000011",
+    "1100000000000100000000000000000000000011",
+    "1100000000000100000000000000000000000011",
+    "1100000000000111111111110000000000000011",
+    "1100000000000000000000010000000000000011",
+    "1100000000000000000000010000000000000011",
+    "1100000000000111111111110000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000000000000000000000000000011",
+    "1100000000000001000000000000000000000011",
+    "1111111111111110001111111000111111111111",
+    "1111111111111110001111111000111111111111");
+
 begin
-  process(clk) begin   
-           if rising_edge(clk) then
-             Vsync <= 0 when 50000<waitRow<56400 else 1; 
-             if waitCol = 161300 then
-               waitCol = 0;
-             end if     
-             if rowCnt = 480 then
-               waitCol++;
-             else
-               Hsync <= 0 when 64 < waitRow < 444 else 1;
-               if RGB = true  and clkCnt = 3 then
-                 vgaRed <= d((31-8*countPixel) downto (29-8*countPixel));
-                 vgaGreen <= d((28-8*countPixel) downto (26-8*countPixel));
-                 vgaBlue <= d((25-8*countPixel) downto (24-8*countPixel));
-                 countPixel++;
-                 pixelSent++;
-                 if a = "1111111111111111" then
-                   a = "0000000000000000"
-                 endif
-                 if countPixel = 4 then -- Vi har hämtat de fyra pixlarna från vectorn 
-                   a <= a + "0000000000000001"; --adderar adressen med 1
-                   countPixel = 0;
-                 end if;
-                 
-                 if pixelSent = 640 then --Vi har skickar ut 640 pixlar/1 rad
-                   RGB = false;
-                 end if
-                   clkCnt = 0;
-               elsif RGB = false then --väntetid mellan rader
-                 waitRow++;
-                 clkCnt++;
-                 if waitRow = 634 then
-                   rowCnt++;
-                   RGB = true;
-                   waitRow = 0;
-                 end if
-               else -- Vi kan bara skicka ut en pixel var fjärde cykel;
-                 clkCnt++;
-               end if;
-             end if;
-             if rst = 1 then -- nollställning
-               rowCnt = 0;
-               RGB = true;
-               pixelSent = 0;
-               clkCnt = 0;
-               waitRow = 0;
-               waitCol = 0;
-               countPixel = 0;
-               a <= "0000000000000000";
-             end if;          
-  end process;
-  
-  gm0: gm port map(a, d);
-end behavioral;
+
+-- **************
+-- **   VGA    **
+-- **************
+
+process(clk) begin -- Counts clockcycels mod 4
+  if rising_edge(clk) then
+    if rst='1' then
+      pixel <= "00";
+    else
+      pixel <= pixel + 1;
+    end if;
+  end if;
+end process;
+
+process(clk) begin -- hs
+  if rising_edge(clk) then
+    if rst = '1' then
+      xctr <= 0;
+    elsif pixel = 3 then
+      if xctr = 799 then
+        xctr <= 0;
+      else
+        xctr <= xctr + 1;
+      end if;
+    end if;
+    --
+    if xctr = 656 then
+      hs <= '0';
+    elsif xctr = 752 then
+      hs <= '1';
+    end if;
+  end if;
+end process;
+
+process(clk) begin -- vs
+  if rising_edge(clk) then
+    if rst='1' then
+      yctr <= 0;
+    elsif xctr = 799 and pixel = 0 then
+      if yctr = 520 then
+        yctr <= 0;
+      else
+        yctr <= yctr + 1;
+      end if;
+      --
+      if yctr = 490 then
+        vs <= '0';
+      elsif yctr = 492 then
+        vs <= '1';
+      end if;
+    end if;
+  end if;
+end process;
+
+h_sync <= hs;
+v_sync <= vs;
+
+process(clk) begin
+  if rising_edge(clk) then
+    if yctr < 479 and xctr < 639 then -- In bounds
+      if game_map(yctr/16)(xctr/16) = '1' then -- Map tile
+        vga_red <= "111";
+        vga_green <= "111";
+        vga_blue <= "11";
+      else
+        vga_red <= "000";
+        vga_green <= "000";
+        vga_blue <= "00";
+      end if;
+    else
+      vga_red <= "000";
+      vga_green <= "000";
+      vga_blue <= "00";
+    end if;
+  end if;
+end process;
+      
+    
+
+-- *****************
+-- **   PICTURE   **
+-- *****************
+
+end;
