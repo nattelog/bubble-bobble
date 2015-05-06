25290d65cada5cb995fd88ce83abed1389b46c2d
Natanael Log <natlo809@student.liu.se>
2015-05-06 15:13:17 +0200

    Jobbar

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -92,8 +92,11 @@ architecture behavioral of cpu is
     X"00000003",
     OP_CMP & GRX_ZERO & M_IMMEDIATE & FILL & X"0000",
     X"00000001",
-    OP_BRA & GRX_ZERO & M_IMMEDIATE & FILL & X"0000",
+    OP_BRA & GRX_ZERO & M_IMMEDIATE & FILL & X"0006",
     HALT,
+    OP_LDA & GRX_ONE & M_DIRECT & FILL & X"0001",
+    OP_ADD & GRX_ONE & M_DIRECT & FILL & X"0003",
+    OP_BRA & GRX_ZERO & M_IMMEDIATE & FILL & X"0005",
     others => (others => '0')
     );
 
